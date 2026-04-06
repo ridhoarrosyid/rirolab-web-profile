024842dd68f08b05e8b745b06289f5f894cc01cf
ridhoarrosyid <muhammadridhoarrosyid@gmail.com>
2026-04-07 00:37:24 +0700

    first init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className='text-4xl min-h-screen flex justify-center items-center'>
+      Project Web Profile Rirolab Init
+    </h1>
+  );
+}
+
+export default App;
